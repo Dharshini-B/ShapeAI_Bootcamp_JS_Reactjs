d1378c6497580355c2734af1e658b6e8212a3760
Dharshini-B <84831320+Dharshini-B@users.noreply.github.com>
2021-06-05 20:20:04 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and Reactjs Bootcamp</h1>
+      <p>
+        I attended all the 7 days sesssion of this amazing bootcamp conducted by
+        ShapeAI. I understood everything taught by Shaurya Sinha sir. Thanks to
+        ShapeAI and Shaurya sir.
+      </p>
+    </div>
+  );
+}
+export default Note;
